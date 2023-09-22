8ca6d845f85e814f85f18f75fb16ee2094a4295a
YVolkov404 <sir.volkov@outlook.com>
2023-09-22 15:30:20 +0000

    Deploying to gh-pages from @ YVolkov404/goit-js-hw-09@773825ef06025d6d3e8ab43d9baa15d25f30dd6a 🚀

---
+++ 01-color-switcher.86368133.js
@@ -0,0 +1,12 @@
+const t=document.querySelector("body"),e=document.querySelector('button[data-start="start"]'),r=document.querySelector('button[data-stop="stop"]');let o=null;e.addEventListener("click",function(e){null!==(o=setInterval(()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3))&&(e.target.setAttribute("disabled",""),r.removeAttribute("disabled"))}),r.addEventListener("click",function(r){clearInterval(o),r.target.setAttribute("disabled",""),t.style.removeProperty("background-color"),e.removeAttribute("disabled");// test(e);
+}),// default availability state for button 'Stop'
+r.setAttribute("disabled","");// function test(e) {
+//   if (e.target.hasAttribute('disabled')) {
+//     e.target.setAttribute('disabled', '');
+//   } else {
+//     e.target.removeAttribute('disabled');
+//   }
+// }
+//# sourceMappingURL=01-color-switcher.86368133.js.map
+
+//# sourceMappingURL=01-color-switcher.86368133.js.map
